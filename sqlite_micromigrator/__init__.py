4f2ee7c6f91c4332b5c847fe94068d6dab9d2294
megahomyak <g.megahomyak@gmail.com>
2025-09-25 19:50:11 +0500

    Fixed some stuff in the lib, added some tests

--- sqlite_micromigrator/__init__.py
+++ sqlite_micromigrator/__init__.py
@@ -4,7 +4,7 @@ def add_column(cursor, table_name, column_name, column_type):
     if column_name not in column_names:
         cursor.execute(f"ALTER TABLE {table_name} ADD COLUMN {column_name} {column_type};")
 
-def remove_column(cursor, table_name, column_name):
+def drop_column(cursor, table_name, column_name):
     cursor.execute(f"PRAGMA table_info({table_name});")
     column_names = [info_row[1] for info_row in cursor.fetchall()]
     if column_name in column_names:
@@ -12,16 +12,16 @@ def remove_column(cursor, table_name, column_name):
 
 class MigrationNotFound(Exception): pass
 
-def migrate(migrate_version, cursor):
-    def get_database_version(cursor):
-        c.execute("PRAGMA user_version;")
-        return c.fetchone()[0]
-    def set_database_version(cursor, new_version):
-        c.execute(f"PRAGMA user_version={new_version};")
+def migrate(cursor, migrate_version):
+    def get_database_version():
+        cursor.execute("PRAGMA user_version;")
+        return cursor.fetchone()[0]
+    def set_database_version(new_version):
+        cursor.execute(f"PRAGMA user_version={new_version};")
     try:
         while True:
-            current_version = get_database_version(cursor)
-            migrate_version(current_version, cursor)
-            set_database_version(current_version + 1, cursor)
+            current_version = get_database_version()
+            migrate_version(cursor, current_version)
+            set_database_version(current_version + 1)
     except MigrationNotFound:
         pass
--- tests.py
+++ tests.py
@@ -1,26 +1,65 @@
-import sqlite_micromigrator
+import sqlite_micromigrator, sqlite3
 
-class SuddenInterruption(Exception): pass
+class SuddenStop(Exception): pass
 
-def make_migrate_version(should_suddenly_interrupt, do_version_1):
-    def migrate_version(version, cursor):
+def make_migrate_version(log, do_sudden_stop=False, version_bound=None):
+    def migrate_version(cursor, version):
+        if version_bound is not None and version >= version_bound:
+            raise sqlite_micromigrator.MigrationNotFound()
         if version == 0:
+            log.append("v0")
             cursor.execute("""
             CREATE TABLE IF NOT EXISTS a (
-                b TEXT
+                b TEXT,
+                c TEXT
             );
             """)
-        elif version == 1 and do_version_1:
-            sqlite_micromigrator.add_column("a", "c", "INTEGER")
-            if should_suddenly_interrupt:
-                raise SuddenInterruption()
-            sqlite_micromigrator.add_column("a", "d", "BLOB")
+        elif version == 1:
+            log.append("v1")
+            sqlite_micromigrator.add_column(cursor, "a", "d", "BLOB")
+            if do_sudden_stop:
+                raise SuddenStop()
+            sqlite_micromigrator.add_column(cursor, "a", "e", "BLOB")
+        elif version == 2:
+            log.append("v2")
+            sqlite_micromigrator.drop_column(cursor, "a", "d")
+            sqlite_micromigrator.drop_column(cursor, "a", "e")
         else:
             raise sqlite_micromigrator.MigrationNotFound()
+    return migrate_version
 
 def main():
-    with sqlite3.connect("test.db") as conn:
-        with conn.cursor() as cursor:
-            sqlite_micromigrator.migrate(migrate_version, cursor)
+    log = []
+    def assert_equal(a, b):
+        try:
+            assert a == b
+        except AssertionError:
+            print(a, b)
+            raise
+    def assert_and_clear_log(expected_log):
+        assert_equal(log, expected_log)
+        log.clear()
+    with sqlite3.connect(":memory:") as conn:
+        cursor = conn.cursor()
+        def assert_column_names(expected_column_names):
+            cursor.execute(f"PRAGMA table_info(a);")
+            actual_column_names = set(info_row[1] for info_row in cursor.fetchall())
+            assert_equal(actual_column_names, expected_column_names)
+        try:
+            sqlite_micromigrator.migrate(cursor, make_migrate_version(log, do_sudden_stop=True))
+        except SuddenStop:
+            pass
+        assert_and_clear_log(["v0", "v1"])
+        assert_column_names({"b", "c", "d"})
+        sqlite_micromigrator.migrate(cursor, make_migrate_version(log, version_bound=2))
+        assert_and_clear_log(["v1"])
+        assert_column_names({"b", "c", "d", "e"})
+        sqlite_micromigrator.migrate(cursor, make_migrate_version(log))
+        assert_and_clear_log(["v2"])
+        assert_column_names({"b", "c"})
+        sqlite_micromigrator.migrate(cursor, make_migrate_version(log, do_sudden_stop=False))
+        assert_and_clear_log([])
+        assert_column_names({"b", "c"})
+        print("All tests passed")
 
 main()
