2218bbb9f40be95f372f8a29b8e1aef2b9ee56e5
megahomyak <g.megahomyak@gmail.com>
2025-09-25 19:06:34 +0500

    More into tests. My battery is at 7%!

--- tests.py
+++ tests.py
@@ -1,10 +1,24 @@
 import sqlite_micromigrator
 
-def migrate_version()
+def make_migrate_version(should_suddenly_interrupt, do_version_1):
+    def migrate_version(version, cursor):
+        if version == 0:
+            cursor.execute("""
+            CREATE TABLE IF NOT EXISTS a (
+                b TEXT
+            );
+            """)
+        elif version == 1 and do_version_1:
+            sqlite_micromigrator.add_column("a", "c", "INTEGER")
+            if should_suddenly_interrupt:
+                raise Exception()
+            sqlite_micromigrator.add_column("a", "d", "BLOB")
+        else:
+            raise sqlite_micromigrator.MigrationNotFound()
 
 def main():
     with sqlite3.connect("test.db") as conn:
-        ... # battery drains out, hold on
-        sqlite_micromigrator.migrate
+        with conn.cursor() as cursor:
+            sqlite_micromigrator.migrate(migrate_version, cursor)
 
 main()
